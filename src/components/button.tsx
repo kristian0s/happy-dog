92bf3756f250c338a912a9fa6b8bc3e64def8d7e
grafika10 <grafika10@ucione.local>
2023-06-09 22:01:15 +0200

    new project, page button.tsx

---
+++ src/components/button.tsx
@@ -0,0 +1,10 @@
+type ButtonProps = {
+  text: string;
+  color?: "red" | "green";
+};
+
+const Button = ({ text, color = "green" }: ButtonProps) => {
+  return <button className={`btn btn--${color}`}>{text}</button>;
+};
+
+export default Button;
